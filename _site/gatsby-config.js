fda2935f21086d5721dd82f429976e51e44f9109
diegopicazo <diegopicazo@archOS.local>
2022-11-15 02:47:56 +0100

    setting page

--- _site/gatsby-config.js
+++ _site/gatsby-config.js
@@ -10,7 +10,7 @@
 module.exports = {
   pathPrefix: "/",
   siteMetadata: {
-    title: `gu4re.github.io`,
+    title: `ER4UG.reverse`.bold,
     description: `IO SITE | gu4re`,
     author: `@gu4re`,
     siteUrl: `https://github.com/gu4re`,
