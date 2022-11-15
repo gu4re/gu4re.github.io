40448476645535ea3b8e41d85a5f159c83fbfdfe
diegopicazo <diegopicazo@archOS.local>
2022-11-15 01:30:37 +0100

    setting page

--- _site/src/components/layout.js
+++ _site/src/components/layout.js
@@ -33,16 +33,6 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
       </div>
     </>
   )
