4ed35d380ebbc8f26e926ae824f8db48d4842553
diegopicazo <diegopicazo@archOS.local>
2022-11-15 03:21:16 +0100

    setting page

--- _site/gatsby-config.js
+++ _site/gatsby-config.js
@@ -10,7 +10,7 @@
 module.exports = {
   pathPrefix: "/",
   siteMetadata: {
-    title: `gu4re.github.io`,
+    title: ``,
     description: `IO SITE | gu4re`,
     author: `@gu4re`,
     siteUrl: `https://github.com/gu4re`,
