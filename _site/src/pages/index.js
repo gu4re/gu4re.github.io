411bb0e2545ba0432c7180b171066c41f07d3f1a
diegopicazo <diegopicazo@archOS.local>
2022-11-21 22:49:26 +0100

    home edited

--- _site/src/pages/index.js
+++ _site/src/pages/index.js
@@ -12,22 +12,6 @@ const IndexPage = () => (
 
 )
 
-
-
-// Please press <b>Enter</b> to continue...
-// LO MEJOR ES PONER LA MANZANA COMO FOTO CUSTOM, LUEGO UN TREE Y LISTO Y QUE PINCHE
-
-/*document.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    window.location.href = "https://gu4re.github.io/page-2/"
-  }
-})*/
-
-
-
-// si se presiona intro entonces
-// window.location.href = "http://example.com/new_url";
-
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Home | Gu4re" />
 
 export default IndexPage
