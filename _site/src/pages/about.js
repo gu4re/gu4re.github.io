574934793127d355ee6289b5b31b4e31a950451b
diegopicazo <diegopicazo@archOS.local>
2022-11-21 23:28:32 +0100

    home edited

---
+++ _site/src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </main>
+    )
+}
+
+export const Head = () => <Seo title="About | Gu4re" />
+
+export default AboutPage
